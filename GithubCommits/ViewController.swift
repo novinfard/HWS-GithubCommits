48b6b82adada4edcec84fa2428c000a703efffbf
Soheil Novinfard <novinfard@gmail.com>
2018-01-17 16:39:05 +0000

    Add NSPersistentContainer

--- GithubCommits/ViewController.swift
+++ GithubCommits/ViewController.swift
@@ -7,17 +7,37 @@
 //
 
 import UIKit
+import CoreData
 
 class ViewController: UITableViewController {
+	
+	var container: NSPersistentContainer!
 
 	override func viewDidLoad() {
 		super.viewDidLoad()
-		// Do any additional setup after loading the view, typically from a nib.
+
+		container = NSPersistentContainer(name: "MyModel")
+		
+		container.loadPersistentStores { (storeDescription, error) in
+			if let error = error {
+				print("Unsolved erro \(error.localizedDescription)")
+			}
+		}
 	}
 
 	override func didReceiveMemoryWarning() {
 		super.didReceiveMemoryWarning()
 		// Dispose of any resources that can be recreated.
+	}
+	
+	func saveContext() {
+		if container.viewContext.hasChanges {
+			do {
+				try container.viewContext.save()
+			} catch {
+         		print("An error occurred while saving: \(error)")
+			}
+		}
 	}
 
 
