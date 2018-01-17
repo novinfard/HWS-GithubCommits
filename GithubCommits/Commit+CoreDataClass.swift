9473034f6393812d357d6b6ed591831b51ec9d0d
Soheil Novinfard <novinfard@gmail.com>
2018-01-17 19:12:05 +0000

    Add NSManagedObject

---
+++ GithubCommits/Commit+CoreDataClass.swift
@@ -0,0 +1,16 @@
+//
+//  Commit+CoreDataClass.swift
+//  GithubCommits
+//
+//  Created by Soheil on 17/01/2018.
+//  Copyright © 2018 Soheil Novinfard. All rights reserved.
+//
+//
+
+import Foundation
+import CoreData
+
+@objc(Commit)
+public class Commit: NSManagedObject {
+
+}
---
+++ GithubCommits/Commit+CoreDataProperties.swift
@@ -0,0 +1,25 @@
+//
+//  Commit+CoreDataProperties.swift
+//  GithubCommits
+//
+//  Created by Soheil on 17/01/2018.
+//  Copyright © 2018 Soheil Novinfard. All rights reserved.
+//
+//
+
+import Foundation
+import CoreData
+
+
+extension Commit {
+
+    @nonobjc public class func createFetchRequest() -> NSFetchRequest<Commit> {
+        return NSFetchRequest<Commit>(entityName: "Commit")
+    }
+
+    @NSManaged public var date: Date
+    @NSManaged public var message: String
+    @NSManaged public var sha: String
+    @NSManaged public var url: String
+
+}
