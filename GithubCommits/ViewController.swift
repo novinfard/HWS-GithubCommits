9b8a12b597148d73c5358aa46f46f6b550b719c8
Soheil Novinfard <novinfard@gmail.com>
2018-01-20 11:37:08 +0000

    Add unique constraint

--- GithubCommits/ViewController.swift
+++ GithubCommits/ViewController.swift
@@ -20,6 +20,8 @@ class ViewController: UITableViewController {
 		container = NSPersistentContainer(name: "GithubCommits")
 		
 		container.loadPersistentStores { (storeDescription, error) in
+			self.container.viewContext.mergePolicy = NSMergeByPropertyObjectTrumpMergePolicy
+			
 			if let error = error {
 				print("Unsolved error \(error.localizedDescription)")
 			}
